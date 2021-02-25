69f38ab5d4aad8e810f24be62b8b2b67d8b3f149
Alston Cobb <atc5nc@virginia.edu>
2021-02-25 12:14:53 -0300

    Created the model, the routes, controller, and view that shows 'Welcome to thje jungle' as the welcome page

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+    def index
+    end
+
+    def create
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def show
+    end
+    
+    def destroy
+    end
+
+    def update
+    end
+end
